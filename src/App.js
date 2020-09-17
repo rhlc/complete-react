bfe309b5c3c626fbdbf42119ef6341e7e504cdab
Rahul Choudhary <rahulcy7@gmail.com>
2020-09-17 20:53:50 +0530

    done for day

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
 import React from 'react';
 import { render } from 'react-dom';
-
-const Pet = ({ name, animal, breed }) => {
-  return React.createElement('div', {}, [
-    React.createElement('h1', {}, name),
-    React.createElement('h2', {}, animal),
-    React.createElement('h2', {}, breed),
-  ]);
-};
+import Pet from './Pet';
 
 const App = () => {
-  return React.createElement('div', { id: 'important' }, [
-    React.createElement('h1', {}, 'Adopt me!'),
-    React.createElement(Pet, { name: 'Luna', animal: 'dog', breed: 'labra' }),
-    React.createElement(Pet, { name: 'Pepper', animal: 'cat', breed: 'tiger' }),
-    React.createElement(Pet, { name: 'Doink', animal: 'rat', breed: 'street' }),
-  ]);
+  return (
+    <div>
+      <h1 id="something important">Adopt me</h1>
+      <Pet name="Luna" animal="Dog" breed="Labra" />
+      <Pet name="Peppa" animal="Bird" breed="Parrot" />
+      <Pet name="Lara" animal="Cat" breed="Tiger" />
+    </div>
+  );
 };
 
-render(React.createElement(App), document.getElementById('root'));
+render(<App />, document.getElementById('root'));
